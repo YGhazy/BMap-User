7d7a57636ebbe09d9c86621e48355a23578081a0
Nader Hosny <n.hosny@boltsmartsolutions.com>
2021-04-14 22:44:08 +0200

    register integration created
    * there is an issue where the form is always valid ... still needs investigation

--- src/app/modules/register/register.component.ts
+++ src/app/modules/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CustomerService } from 'src/app/services/customer.service';
 import { RegisterModel } from '../../models/auth-models/RegisterModel';
 import { AuthenticationService } from '../../services/authentication.service';
 import { formBuilderHelper } from '../../services/utilities/formBuilderHelper';
@@ -12,42 +13,57 @@ import { formBuilderHelper } from '../../services/utilities/formBuilderHelper';
 })
 export class RegisterComponent implements OnInit {
   //handling show/hide password
-  signupForm;
   inputType: string = "password";
   inputConfirmType: string = "password";
   pView: string = "-slash";
   pViewConfirmPassword: string = "-slash";
-  currentPage: number = 1; // current page indicator 'replaced next1/2/3 booleans'
+  currentPage: number = 1; // current page indicator
   isLoading: boolean = false;
   flag: boolean = false;
   //dropdown variables
   stValue: string;
-  genderValue: string;
   showOptions: boolean = false;
   show: boolean = true;
 
-  constructor(private formBuilderHelper: formBuilderHelper, private AuthenticationService: AuthenticationService, private router: Router) {
+  //gender selection variable
+  selectedGender: string;
+
+  //FormGroup
+  signupForm;
+
+  registerationResult: boolean;
+  constructor(private formBuilderHelper: formBuilderHelper, private AuthenticationService: AuthenticationService, private customerService: CustomerService, private router: Router) {
     this.signupForm = this.formBuilderHelper.CustomizeFormbuilderValidator({
-      mail: '', password: '', confirmPassword: '',
-      firstMiddleName: '', firstName: '', SecondMiddleName: '',
-      lastName: '', phoneNumber: '', nationalID: '', gender: ''
-
-    }, this.checkPasswords)
-
+      password: '',
+      confirmPassword: '',
+      firstName: '',
+      firstMiddleName: '',
+      secondMiddleName: '',
+      lastName: '',
+      email: '',
+      mobileNumber: '',
+      nationalID: '',
+      gender: '',
+      dateOfBirth: '',
+      jobTitle: '',
+      address: '',
+      city: '',
+      province: '',
+      country: '',
+      accountType: '',
+    }, this.checkPasswords);
   }
   accountType_values = [
     { id: 0, value: "Individual" },
     { id: 1, value: "Company" },
   ];
-  gender_values = [
-    { id: 0, value: "Male" },
-    { id: 1, value: "Female" },
-  ];
   ngOnInit(): void {
   }
 
-  showOption() {
 
+  //Account type selection section
+  //Display account type downdown list
+  showOption() {
     if (this.flag == false) {
       this.showOptions = true;
       this.show = false;
@@ -58,79 +74,80 @@ export class RegisterComponent implements OnInit {
       this.show = true;
       this.flag = false;
     }
-
   }
+  hideOption() {
+    this.showOptions = false;
+    this.show = true;
+    this.flag = false;
+  }
+
+  //Fetch account type dropdown item value and select it
   getAccountTypeValue(id: any) {
     this.stValue = this.accountType_values.find(b => b.id == id).value;
     this.showOptions = false;
     this.show = true;
   }
 
-  getGenderValue(id: any) {
-    this.genderValue = this.gender_values.find(b => b.id == id).value;
-    this.showOptions = false;
-    this.show = true;
+  //Gender selection 'Appends value to gender formControl, adds highlighted ngClass to selected gender icon
+  SelectGender(selector: string) {
+    this.signupForm.value.gender = selector;
+    this.selectedGender = selector;
   }
 
-  hideOption() {
-
-    this.showOptions = false;
-    this.show = true;
-    this.flag = false;
-  }
-
+  //Customer registeration api call
   Register() {
-
+    var currentDate = new Date;
     const RegisterModel: RegisterModel = {
-      gender: "",
-      email: this.signupForm.mail.value,
-      phoneNumber: this.signupForm.phoneNumber.value,
-      nationalID: this.signupForm.phoneNumber.value,
-      dateOfBirth: this.signupForm.phoneNumber.value,
-      jobTitle: this.signupForm.phoneNumber.value,
-      type: this.signupForm.phoneNumber.value,
-      accountStatus: this.signupForm.phoneNumber.value,
-      city: this.signupForm.phoneNumber.value,
-      province: this.signupForm.phoneNumber.value,
-      country: this.signupForm.phoneNumber.value,
-      street: this.signupForm.phoneNumber.value,
+      gender: this.signupForm.value.gender,
+      email: this.signupForm.value.email,
+      phoneNumber: this.signupForm.value.mobileNumber,
+      nationalID: this.signupForm.value.nationalID,
+      dateOfBirth: currentDate,
+      jobTitle: this.signupForm.value.jobTitle,
+      type: this.signupForm.value.accountType,
+      accountStatus: 'pending',
+      city: this.signupForm.value.city,
+      province: this.signupForm.value.province,
+      country: this.signupForm.value.country,
+      street: null,
       nationalIdFront: "",
       nationalIdBack: "",
       profilePicture: "",
-      first: this.signupForm.phoneNumber.value,
-      firstMiddle: this.signupForm.phoneNumber.value,
-      secondMiddle: this.signupForm.phoneNumber.value,
-      last: this.signupForm.phoneNumber.value,
-      password: this.signupForm.phoneNumber.value
+      first: this.signupForm.value.firstName,
+      firstMiddle: this.signupForm.value.firstMiddleName,
+      secondMiddle: this.signupForm.value.SecondMiddleName,
+      last: this.signupForm.value.lastName,
+      password: this.signupForm.value.password
     }
     console.log(RegisterModel)
 
-    this.AuthenticationService.login(RegisterModel).subscribe(res => {
+    this.customerService.Register(RegisterModel).subscribe(res => {
       if (res.succeeded) {
-        this.router.navigate(["/home"]);
-        this.AuthenticationService.setToken(res.data.token);
-        console.log("login")
-
+        console.log("registered ", res.data);
+        this.registerationResult = true;
       }
     }, error => {
+      this.registerationResult = false;
       console.log(error);
       this.isLoading = false;
     });
   }
 
   //Page navigation
-
   Back() {
-    if(this.currentPage > 1){
+    if (this.currentPage > 1) {
       this.currentPage = this.currentPage - 1;
     }
   }
   Next() {
-    if(this.currentPage < 3){
+    if (this.currentPage < 3) {
       this.currentPage = this.currentPage + 1;
     }
   }
-  
+
+
+  //Password section
+  //View/Hide password
   viewPassword() {
     if (this.inputType != 'text') {
       this.inputType = 'text';
@@ -151,7 +168,6 @@ export class RegisterComponent implements OnInit {
     else {
       this.inputConfirmType = 'password';
       this.pViewConfirmPassword = "-slash";
-
     }
   }
 
@@ -161,9 +177,8 @@ export class RegisterComponent implements OnInit {
     let confirmPass = group.get('confirmPassword').value;
     return pass === confirmPass ? false : { notSame: true }
   }
-  get f() {
+
+  get signupFormControls() {
     return this.signupForm.controls;
   }
-
-
 }
